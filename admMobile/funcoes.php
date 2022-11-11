326e4312587a88e83ed8ae84672b55761eac5913
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2022-11-10 22:58:31 -0300

    mudando layout e css da página principal

--- admMobile/funcoes.php
+++ admMobile/funcoes.php
@@ -13,7 +13,7 @@
     $res = $GLOBALS['conn']->query($sql);
 
     if($res){
-      echo '<h3 id="textDefault">Categoria cadastrada</h3>';
+      echo '<h3 id="textDefault">Categoria cadastrada com sucesso!!!</h3>';
     } else{
       echo '<h3 id="textDefault">Erro ao cadastrar categoria</h3>';
     }
@@ -28,28 +28,42 @@
       echo '<h3 id="textDefault">Erro ao editar categoria</h3>';
     }
   }
-  function MostrarCategorias(){
+  function MostrarCategoria(){
     $sql = 'SELECT * FROM categoria';
     $res = $GLOBALS['conn']->query($sql);
 
     if($res -> num_rows > 0){
       while($row = $res->fetch_assoc()){
         echo '
-        <div class="card1">
-        <h3 id="textDefault">Código: '.$row['cd'].'</h3>
-        <h3 id="textDefault">Categoria: '.$row['nome'].'</h3>
-        <span>
-          <script>
-            function editarCat(){
-              document.getElementById("editarCat").style.display = "flex";
-            }
-          </script>
-          <a href="index.php"><img src="img/lixeira.png" width="25px"/></a>
-          <a onclick="editarCat()" href="#editarCat"><img src="img/escrever.png" width="25px"/></a>
-        </span>
-      </div>
+              <tr>
+                <td id="td">'.$row['cd'].'</td>
+                <td id="td">'.$row['nome'].'</td>
+                <td id="td">
+                  <a href="" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
         ';
       }
+    } else{
+      echo '
+              <tr>
+                <td id="td">Null</td>
+                <td id="td">Null</td>
+                <td id="td">
+                  <a href="" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
+        ';
     }
   }
   function MostrarCategoriaSelect(){
@@ -59,9 +73,7 @@
     if($res -> num_rows > 0){
       while($row = $res->fetch_assoc()){
         echo '
-              <h3>
                 <option value="'.$row['cd'].'">'.$row['nome'].'</option>
-              </h3>
         ';
       }
     }
@@ -70,13 +82,49 @@
     $sql = 'INSERT INTO produto(cd_categoria, cd_produto, ds_produto, imagem, link, nome, valor) VALUES ("'.$cdCat.'", null, "'.$descProd.'", "'.$imagem.'", "'.$link.'", "'.$nome.'", "'.$valor.'")';
     $res = $GLOBALS['conn']->query($sql);
     if($res){
-      echo '<script>
-        alert("Produto cadastrado com sucesso");
-      </script>';
+      echo '<h3 id="textDefault">Produto cadastrado com sucesso!!!</h3>';
     } else{
-      echo '<script>
-        alert("Erro ao cadastrar produto");
-      </script>';
+      echo '<h3 id="textDefault">Erro ao cadastrar produto!!!</h3>';
+    }
+  }
+  function MostrarProduto(){
+    $sql = 'SELECT * FROM produto';
+    $res = $GLOBALS['conn']->query($sql);
+
+    if($res -> num_rows > 0){
+      while($row = $res->fetch_assoc()){
+        echo '
+              <tr>
+                <td id="td">'.$row['cd_produto'].'</td>
+                <td id="td">'.$row['nome'].'</td>
+                <td id="td">'.$row['valor'].'</td>
+                <td id="td">
+                  <a href="" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
+        ';
+      }
+    } else{
+      echo '
+              <tr>
+                <td id="td">Null</td>
+                <td id="td">Null</td>
+                <td id="td">Null</td>
+                <td id="td">
+                  <a href="" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
+        ';
     }
   }
   function JsonCategoria(){

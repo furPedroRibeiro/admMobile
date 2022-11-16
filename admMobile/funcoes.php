4d993e116e3fd2c4209278c4046f48d211b087c1
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2022-11-15 22:52:03 -0300

    Back end em desenvolvimento final

--- admMobile/funcoes.php
+++ admMobile/funcoes.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
   $user = 'root';
   $pass = '';
   $banco = 'vitrine';
@@ -13,19 +14,23 @@
     $res = $GLOBALS['conn']->query($sql);
 
     if($res){
-      echo '<h3 id="textDefault">Categoria cadastrada com sucesso!!!</h3>';
+      echo '<h3 id="removeu" class="textDefault">Categoria cadastrada com sucesso(o site será atualizado em 7 segundos)</h3>';
+        $_SESSION['redirect'] = 1;
     } else{
-      echo '<h3 id="textDefault">Erro ao cadastrar categoria</h3>';
+      echo '<h3 id="removeu" class="textDefault">Erro ao cadastrar categoria(o site será atualizado em 7 segundos)</h3>';
+        $_SESSION['redirect'] = 1;
     }
   }
-  function EditarCategoria($cat, $name){
+  function EditarCategoria($name, $cat){
     $sql = 'UPDATE categoria SET nome = "'.$name.'" WHERE cd ='.$cat;
     $res = $GLOBALS['conn']->query($sql);
 
     if($res){
-      echo '<h3 id="textDefault">Categoria editada</h3>';
+      echo '<h3 class="textDefault">Categoria editada com sucesso(o site será atualizado em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
     } else{
-      echo '<h3 id="textDefault">Erro ao editar categoria</h3>';
+      echo '<h3 class="textDefault">Categoria não foi editada(o site será atualizado em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
     }
   }
   function MostrarCategoria(){
@@ -34,27 +39,47 @@
 
     if($res -> num_rows > 0){
       while($row = $res->fetch_assoc()){
-        echo '
+        if($row['cd'] % 2 == 1){
+          echo '
               <tr>
-                <td id="td">'.$row['cd'].'</td>
-                <td id="td">'.$row['nome'].'</td>
-                <td id="td">
-                  <a href="" style="text-decoration: none">
+                <td id="tdCat">'.$row['cd'].'</td>
+                <td id="tdCat">'.$row['nome'].'</td>
+                <td id="tdCat">
+                  <a href="index.php?removeCat='.$row['cd'].'&nomeCat='.$row['nome'].'" style="text-decoration: none">
                     <img src="img/lixeira.png" alt="" width="25px" />
                   </a>
-                  <a href="" style="text-decoration: none">
+                  <a href="#editar" onclick="mostrarEdit()" style="text-decoration: none">
                     <img src="img/escrever.png" alt="" width="25px" />
                   </a>
                 </td>
               </tr>
         ';
+        } else{
+          echo '
+              <tr>
+                <td id="tdCat" style="background-color: var(--fundo); color: var(--corPrimaria)">'.$row['cd'].'</td>
+                <td id="tdCat" style="background-color: var(--fundo); color: var(--corPrimaria)">'.$row['nome'].'</td>
+                <td id="tdCat" style="background-color: var(--fundo); color: var(--corPrimaria)">
+                  <a href="index.php?removeCat='.$row['cd'].'&nomeCat='.$row['nome'].'" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="#editar" onclick="mostrarEdit()" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
+        ';
+        }
+      }
+      if(isset($_GET['removeCat'])){
+        ExcluirCategoria($_GET['removeCat'], $_GET['nomeCat']);
       }
     } else{
       echo '
               <tr>
-                <td id="td">Null</td>
-                <td id="td">Null</td>
-                <td id="td">
+                <td id="tdCat">Null</td>
+                <td id="tdCat">Null</td>
+                <td id="tdCat">
                   <a href="" style="text-decoration: none">
                     <img src="img/lixeira.png" alt="" width="25px" />
                   </a>
@@ -78,13 +103,26 @@
       }
     }
   }
+  function ExcluirCategoria($cd, $nome){
+      $sql = 'DELETE FROM categoria WHERE cd ='.$cd;
+      $res = $res = $GLOBALS['conn']->query($sql);
+      if($res){
+        echo '<h3 class="textDefault">Categoria excluída com sucesso(o site será atualizado em 7 segundos)</h3>';
+        $_SESSION['redirect'] = 1;
+      } else{
+        echo '<h3 class="textDefault">Categoria não foi excluída com sucesso(o site será atualizado em 7 segundos)</h3>';
+        $_SESSION['redirect'] = 1;
+      }
+  }
   function CadastrarProduto($cdCat, $descProd, $imagem, $link, $nome, $valor){
     $sql = 'INSERT INTO produto(cd_categoria, cd_produto, ds_produto, imagem, link, nome, valor) VALUES ("'.$cdCat.'", null, "'.$descProd.'", "'.$imagem.'", "'.$link.'", "'.$nome.'", "'.$valor.'")';
     $res = $GLOBALS['conn']->query($sql);
     if($res){
-      echo '<h3 id="textDefault">Produto cadastrado com sucesso!!!</h3>';
+      echo '<h3 id="textDefault">Produto cadastrado com sucesso!!!(o site será atualizado em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
     } else{
-      echo '<h3 id="textDefault">Erro ao cadastrar produto!!!</h3>';
+      echo '<h3 id="textDefault">Erro ao cadastrar produto!!!(o site será atualizado em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
     }
   }
   function MostrarProduto(){
@@ -93,13 +131,14 @@
 
     if($res -> num_rows > 0){
       while($row = $res->fetch_assoc()){
-        echo '
+        if($row['cd_produto'] % 2 == 1){
+          echo '
               <tr>
-                <td id="td">'.$row['cd_produto'].'</td>
-                <td id="td">'.$row['nome'].'</td>
-                <td id="td">'.$row['valor'].'</td>
-                <td id="td">
-                  <a href="" style="text-decoration: none">
+                <td id="tdProd">'.$row['cd_produto'].'</td>
+                <td id="tdProd">'.$row['nome'].'</td>
+                <td id="tdProd">'.$row['valor'].'</td>
+                <td id="tdProd">
+                  <a href="index.php?removeProd='.$row['cd_produto'].'&nomeProd='.$row['nome'].'" style="text-decoration: none">
                     <img src="img/lixeira.png" alt="" width="25px" />
                   </a>
                   <a href="" style="text-decoration: none">
@@ -108,14 +147,34 @@
                 </td>
               </tr>
         ';
+        } else{
+          echo '
+              <tr>
+                <td id="tdProd" style="background-color: var(--fundo); color: var(--corPrimaria)">'.$row['cd_produto'].'</td>
+                <td id="tdProd" style="background-color: var(--fundo); color: var(--corPrimaria)">'.$row['nome'].'</td>
+                <td id="tdProd" style="background-color: var(--fundo); color: var(--corPrimaria)">'.$row['valor'].'</td>
+                <td id="tdProd" style="background-color: var(--fundo); color: var(--corPrimaria)">
+                  <a href="index.php?removeProd='.$row['cd_produto'].'&nomeProd='.$row['nome'].'" style="text-decoration: none">
+                    <img src="img/lixeira.png" alt="" width="25px" />
+                  </a>
+                  <a href="" style="text-decoration: none">
+                    <img src="img/escrever.png" alt="" width="25px" />
+                  </a>
+                </td>
+              </tr>
+        ';
+        }
+      }
+      if(isset($_GET['removeProd'])){
+        ExcluirProduto($_GET['removeProd'], $_GET['nomeProd']);
       }
     } else{
       echo '
               <tr>
-                <td id="td">Null</td>
-                <td id="td">Null</td>
-                <td id="td">Null</td>
-                <td id="td">
+                <td id="tdProd">Null</td>
+                <td id="tdProd">Null</td>
+                <td id="tdProd">Null</td>
+                <td id="tdProd">
                   <a href="" style="text-decoration: none">
                     <img src="img/lixeira.png" alt="" width="25px" />
                   </a>
@@ -127,6 +186,17 @@
         ';
     }
   }
+  function ExcluirProduto($cd, $nome){
+    $sql = 'DELETE FROM produto WHERE cd_produto ='.$cd;
+    $res = $GLOBALS['conn']->query($sql);
+    if($res){
+      echo '<h3 id="removeu" class="textDefault">Produto excluído com sucesso(a página será atualizada em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
+    } else{
+      echo '<h3 id="removeu" class="textDefault">Produto não foi excluído com sucesso(o site será atualizado em 7 segundos)</h3>';
+      $_SESSION['redirect'] = 1;
+    }
+}
   function JsonCategoria(){
     $sql = 'SELECT * FROM categoria';
     $res = $GLOBALS['conn']->query($sql);
--- admMobile/index.php
+++ admMobile/index.php
@@ -1,6 +1,8 @@
 <?php
     include('funcoes.php');
-    session_start();
+    if($_SESSION['redirect'] == 1){
+      header("Refresh: 7 ; url = index.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -10,7 +12,360 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Administração</title>
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon" />
-    <link rel="stylesheet" href="style.css" />
+    <style>
+/* CSS geral */
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  overflow-x: hidden;
+  color: var(--corSecundaria);
+}
+:root{
+  --corPrimaria: rgb(255, 255, 255);
+  --corSecundaria: rgb(139, 92, 192);
+  --fundo: rgba(179, 153, 209, 0.741);
+}
+.textDefault{
+  color: var(--corSecundaria);
+  font-size: 22px;
+  font-weight: 450;
+}
+#textDefault{
+  color: var(--corSecundaria);
+  font-size: 22px;
+  font-weight: 450;
+}
+#tituloDefault{
+  color: var(--corSecundaria);
+  font-size: 35px;
+  font-weight: 1000;
+}
+a {
+  text-decoration: none;
+  color: var(--corSecundaria);
+}
+
+/* CSS do header */
+
+.header {
+  background-color: white;
+  color: var(--corSecundaria);
+
+  height: 12vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.header .textLogo h3 a {
+  text-decoration: none;
+  color: var(--corSecundaria);
+}
+#textLogo {
+  font-family: 'Freehand', cursive;
+  font-size: 27px;
+  font-weight: 450;
+}
+
+/* CSS do content */
+
+.content{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.principal{
+  height: 88vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.esquerda{
+  width: 50vw;
+  height: 70vh;
+
+  gap: 3rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  text-align: justify;
+
+  padding: 10vw;
+
+  border-right: 1px solid var(--corSecundaria);
+}
+.direita{
+  width: 50vw;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.button{
+  width: 27vw;
+  height: 6vh;
+
+  color: var(--corPrimaria);
+  background-color: var(--corSecundaria);
+  border: 1px solid var(--corSecundaria);
+  
+  box-shadow: 5px 5px 4px rgb(129, 63, 206);
+
+  transition: width 0.9s;
+}
+.button:hover{
+  color: var(--corSecundaria);
+  background-color: var(--corPrimaria);
+
+  width: 30vw;
+}
+
+/* CSS da adm */
+
+.administracao{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.headerAdm{
+  height: 12vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 4vw;
+}
+.contentAdm{
+  display: flex;
+  flex-direction: column;
+
+  gap: 2rem;
+
+  justify-content: center;
+  align-items: center;
+}
+
+/* CSS categoria e produtos */
+.borboleta{
+  position: absolute;
+  margin-left: -70vw;
+  margin-top: -60vh;
+}/*
+.borboleta2{
+  position: absolute;
+  margin-left: 26vw;
+  margin-top: -20vh;
+}*/
+#categoria{
+  display: flex;
+  flex-direction: column;
+
+  gap: 2rem;
+
+  transition: all 1s;
+
+  justify-content: center;
+  align-items: center;
+}
+#produto{
+  display: none;
+  flex-direction: column;
+
+  gap: 2rem;
+
+  justify-content: center;
+  align-items: center;
+}
+#editar{
+  display: none;
+}
+#optAdmProd{
+  color: var(--corSecundaria);
+
+  cursor: pointer;
+
+  padding: 1vh;
+}
+#optAdmCat{
+  color: var(--corSecundaria);
+
+  border-bottom: 3px solid var(--corSecundaria);
+
+  cursor: pointer;
+
+  padding: 1vh;
+}
+
+/* CSS tables */
+
+table{
+  width: 45vw;
+  border: 1px solid var(--corSecundaria);
+  box-shadow: 5px 5px 4px rgb(129, 63, 206);
+}
+table #thCat{
+  width: 15vw;
+  padding: 0.8rem;
+
+  color: var(--corPrimaria);
+  background-color: var(--fundo);
+}
+table #tdCat{
+  width: 15vw;
+  padding: 0.8rem;
+
+  color: var(--corSecundaria);
+
+  border-bottom: 1px solid var(--corSecundaria);
+  border-right: 1px solid var(--corSecundaria)
+}
+table #thProd{
+  width: 11.25vw;
+  padding: 0.8rem;
+
+  color: var(--corPrimaria);
+  background-color: var(--fundo);
+}
+table #tdProd{
+  width: 11.25vw;
+  padding: 0.8rem;
+
+  color: var(--corSecundaria);
+
+  border-bottom: 1px solid var(--corSecundaria);
+  border-right: 1px solid var(--corSecundaria)
+}
+
+/* CSS form */
+
+.form{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  gap: 2rem;
+}
+.form #titleForm{
+  color: var(--corSecundaria);
+}
+.input{
+  width: 41vw;
+  height: auto;
+
+  outline: none;
+  padding: 7px;
+
+  background-color: var(--fundo);
+  border: 1px solid var(--corSecundaria);
+
+  transition: width 0.7s;
+}
+.input:focus{
+  background-color: var(--corSecundaria);
+  width: 45vw;
+}
+.input::placeholder{
+  color: var(--corSecundaria);
+}
+.input:focus::placeholder{
+  color: var(--corPrimaria);
+}
+input::file-selector-button {
+  width: 10vw;
+
+  color: var(--corSecundaria);
+  background-color: var(--fundo);
+  
+  padding: 0.5em;
+  
+  border: 1px solid var(--corSecundaria);
+
+  transition: width 1s;
+  cursor: pointer;
+}
+input::file-selector-button:hover{
+  width: 13vw;
+  color: var(--corPrimaria);
+  background-color: var(--corSecundaria);
+}
+.form .button{
+  background-color: var(--corSecundaria);
+  color: var(--corPrimaria);
+
+  width: 41vw;
+  transition: width 0.7s;
+}
+.form .button:hover{
+  width: 45vw;
+  background-color: var(--corPrimaria);
+  color: var(--corSecundaria);
+}
+.form select{
+  width: 41vw;
+  height: 3.5vh;
+
+  outline: none;
+  padding: 3px;
+
+  background-color: var(--fundo);
+  color: var(--corSecundaria);
+  border: 1px solid var(--corSecundaria);
+
+  transition: width 0.7s;
+}
+.form select:focus{
+  width: 45vw;
+  background-color: var(--corSecundaria);
+  color: var(--corPrimaria);
+}
+
+/* CSS do footer */
+
+.footer{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  flex-wrap: wrap;
+
+  width: 100vw;
+  gap: 2vh;
+  padding: 2rem;
+}
+
+.footer #textFooter{
+  color: var(--corSecundaria);
+  font-weight: 450;
+}
+.footer .icons{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 12rem;
+}
+#textFooter{
+  font-family: 'Freehand', cursive;
+  font-size: 30px;
+  font-weight: 450;
+}
+    </style>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -49,6 +404,12 @@
         document.getElementById('produto').style.display = 'flex'
         document.getElementById('categoria').style.display = 'none'
       }
+      function mostrarEdit(){
+        document.getElementById('editar').style.display = 'flex'
+      }
+      function zerandoTexts(){
+        document.getElementById('removeu').innerHTML = ''
+      }
     </script>
   </head>
   <body>
@@ -73,6 +434,9 @@
         </div>
       </div>
       <div class="administracao">
+        <div class="borboleta">
+          <img src="img/fundoEsquerda.png" alt="" width="400px" />
+        </div>
         <div class="headerAdm">
           <h3 id="optAdmCat" onclick="categoriaOpt()">Categorias</h3>
           <h3 id="optAdmProd" onclick="produtoOpt()">Produtos</h3>
@@ -99,15 +463,48 @@
                 }
               ?>
             </form>
+            <div id="editar">
+            <form action="" method="post" class="form">
+              <h3 id="titleForm">Editar categoria</h3>
+              <select name="selectCat" id="selectCat">
+                <?php
+                    MostrarCategoriaSelect();
+                ?>
+              </select>
+              <input
+                type="text"
+                name="nomeEditCat"
+                id="nomeEditCat"
+                placeholder="Digite o novo nome da categoria:"
+                class="input"
+              />
+              <a href="zapPedro">
+                <button type="submit" name="enviarCategoria" class="button">
+                  Enviar
+                </button>
+              </a>
+              <?php
+                if(isset($_POST['nomeEditCat'])){
+                    EditarCategoria($_POST['nomeEditCat'], $_POST['selectCat']);
+                }
+              ?>
+            </form>
+            </div>
             <table>
               <tr>
-                <th id="th" style="border-right: 1px solid var(--corPrimaria)">
+                <th
+                  id="thCat"
+                  style="border-right: 1px solid var(--corPrimaria)"
+                >
                   Código
                 </th>
-                <th id="th" style="border-right: 1px solid var(--corPrimaria)">
+                <th
+                  id="thCat"
+                  style="border-right: 1px solid var(--corPrimaria)"
+                >
                   Nome
                 </th>
-                <th id="th">Opções</th>
+                <th id="thCat">Opções</th>
               </tr>
               <?php
                 MostrarCategoria();
@@ -117,7 +514,9 @@
           <div id="produto">
             <form action="" method="post" class="form">
               <h3 id="titleForm">Cadastrar produto</h3>
-              <label for="descProd" id="textDefault">Categoria do produto:</label>
+              <label for="descProd" id="textDefault"
+                >Categoria do produto:</label
+              >
               <select name="selectCat" id="selectCat">
                 <?php
                     MostrarCategoriaSelect();
@@ -158,7 +557,7 @@
                 class="input"
               />
               <a href="zapPedro">
-                <button type="submit" name="enviarCategoria" class="button">
+                <button onclick="produtoOpt()" type="submit" name="enviarCategoria" class="button">
                   Enviar
                 </button>
               </a>
@@ -168,19 +567,29 @@
                     CadastrarProduto($_POST['selectCat'], $_POST['descProd'], $file, $_POST['linkProd'], $_POST['nomeProd'], $_POST['valor']);
                 }
               ?>
+              <h3 id="removeuProd" class="textDefault"></h3>
             </form>
             <table>
               <tr>
-                <th id="th" style="border-right: 1px solid var(--corPrimaria)">
+                <th
+                  id="thProd"
+                  style="border-right: 1px solid var(--corPrimaria)"
+                >
                   Código
                 </th>
-                <th id="th" style="border-right: 1px solid var(--corPrimaria)">
+                <th
+                  id="thProd"
+                  style="border-right: 1px solid var(--corPrimaria)"
+                >
                   Nome
                 </th>
-                <th id="th" style="border-right: 1px solid var(--corPrimaria)">
+                <th
+                  id="thProd"
+                  style="border-right: 1px solid var(--corPrimaria)"
+                >
                   Valor
                 </th>
-                <th id="th">Opções</th>
+                <th id="thProd">Opções</th>
               </tr>
               <?php
                 MostrarProduto();
@@ -188,7 +597,13 @@
             </table>
           </div>
         </div>
-        <div class="footerAdm"></div>
+        <div class="footerAdm">
+        <!-- 
+        <div class="borboleta2">
+          <img src="img/fundoEsquerda.png" alt="" width="400px" />
+        </div>
+        -->
+        </div>
       </div>
     </div>
     <div class="footer">
